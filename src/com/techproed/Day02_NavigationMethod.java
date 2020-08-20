91bf7bed8da5fc7d22c348b165244ac2f71a021f
Onur <onurnis69@gmail.com>
2020-08-20 04:48:14 +0300

    day03 ders sonu

--- src/com/techproed/Day02_NavigationMethod.java
+++ src/com/techproed/Day02_NavigationMethod.java
@@ -3,7 +3,7 @@ package com.techproed;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class Day02NavigationMethod {
+public class Day02_NavigationMethod {
     public static void main(String[] args) {
         System.setProperty("webdriver.chrome.driver" , "C:\\Users\\onurn\\Documents\\selenium dependencies\\drivers\\chromedriver.exe");
 
