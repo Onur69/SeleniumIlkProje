a0753e99ddf38a736962a574b1674b05ad6a4af4
Onur <onurnis69@gmail.com>
2020-08-21 23:16:58 +0300

    Day05_Xpath_CssSelector

--- src/com/techproed/Day02_Homework.java
+++ src/com/techproed/Day02_Homework.java
@@ -3,7 +3,7 @@ package com.techproed;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class Homework {
+public class Day02_Homework {
     public static void main(String[] args) {
         System.setProperty("webdriver.chrome.driver", "C:\\Users\\onurn\\Documents\\selenium dependencies\\drivers\\chromedriver.exe");
         //1- driver'a gidelim
